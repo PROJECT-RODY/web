b4c1c15fb8dc69bdd46ca1ecf1d1ad404c738b38
Minchan kim <minchan1472@naver.com>
2021-10-18 15:47:59 +0900

    update

--- send_email.py
+++ send_email.py
@@ -7,4 +7,12 @@ with open('./email_info.json') as f:
     SETTING = json.load(f)
 
 
-print(SETTING)
+print(SETTING.SERVER)
+
+smtp_server = smtplib.SMTP(host='smtp.gmail.com', port=587) # SMTP 서버 TSL방식으로 접속
+
+hello_message = smtp_server.ehlo() # SMTP서버에 hello 메시지 보냄
+print(hello_message,"\n")
+
+bye_message = smtp_server.quit() # 종료
+print(bye_message)
